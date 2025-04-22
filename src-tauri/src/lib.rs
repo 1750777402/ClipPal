778c6b94891420f026241517bf601d446284df73
张晓 <1750777402@qq.com>
2025-04-22 16:34:04 +0800

    init

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,5 @@
+use tauri::{Manager, PhysicalPosition, PhysicalSize};
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -8,7 +10,30 @@ fn greet(name: &str) -> String {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+        .setup(|app| {
+            // 获取主显示器
+            let main_window = app.get_webview_window("main").unwrap();
+            // 获取主显示器信息
+            let monitor = main_window
+                .primary_monitor()
+                .expect("Failed to get primary monitor")
+                .expect("No primary monitor found");
+
+            // 获取显示器参数
+            let screen_size = monitor.size();
+            let screen_height = screen_size.height as i32;
+            let y_position = (screen_height - 490).max(0);
+
+            // 设置窗口参数
+            main_window.set_size(PhysicalSize::new(screen_size.width, 500))?;
+            main_window.set_position(PhysicalPosition::new(0, y_position))?;
+            // 延迟显示
+            std::thread::sleep(std::time::Duration::from_millis(100));
+            main_window.show().unwrap();
+
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .expect("error running tauri");
 }
