5cd6541f1580e9f4e5568184fd7f5930b4ef9317
张晓 <1750777402@qq.com>
2025-04-23 14:36:07 +0800

    系统托盘区相关处理

--- src-tauri/src/tray.rs
+++ src-tauri/src/tray.rs
@@ -1,70 +1,44 @@
-use tauri::{
-    tray::{MouseButton, MouseButtonState, TrayIconBuilder, TrayIconEvent},
-    Emitter, Manager, PhysicalPosition, Runtime,
-};
 use tauri::image::Image;
+use tauri::{tray::TrayIconBuilder, Manager, Runtime};
+use tauri::menu::{Menu, MenuItem};
+use tauri::tray::{MouseButton, TrayIconEvent};
 
 pub fn create_tray<R: Runtime>(app: &tauri::AppHandle<R>) -> tauri::Result<()> {
     // 为系统创建托盘图标
     let icon = Image::from_bytes(include_bytes!("../icons/icon.png"))?;
+    let quit_i = MenuItem::with_id(app, "quit", "退出", true, None::<&str>)?;
+    let set_sys = MenuItem::with_id(app, "setSys", "设置", true, None::<&str>)?;
+    let menu = Menu::with_items(app, &[&set_sys, &quit_i])?;
     let _ = TrayIconBuilder::with_id("tray")
         .tooltip("ClipPal")
         .icon(icon)
-        .on_tray_icon_event(|tray, event| match event {
-            TrayIconEvent::Click {
-                id: _,
-                position,
-                rect: _,
-                button,
-                button_state,
-            } => match button {
-                MouseButton::Left => {
-                    let windows = tray.app_handle().webview_windows();
-                    for (name, window) in windows {
-                        if name == "login" || name == "home" {
-                            window.show().unwrap();
-                            window.unminimize().unwrap();
-                            window.set_focus().unwrap();
-                            break;
-                        }
-                    }
-                }
-                MouseButton::Right if MouseButtonState::Down == button_state => {
-                    // 状态栏图标按下右键时显示状态栏菜单
-                    let tray_window = tray.app_handle().get_webview_window("tray").unwrap();
-                    if let Ok(outer_size) = tray_window.outer_size() {
-                        tray_window
-                            .set_position(PhysicalPosition::new(
-                                position.x,
-                                position.y - outer_size.height as f64,
-                            ))
-                            .unwrap();
-                        tray_window.set_always_on_top(true).unwrap();
-                        tray_window.show().unwrap();
-                        tray_window.set_focus().unwrap();
-                    }
-                }
-                _ => {}
-            },
-            #[cfg(target_os = "windows")]
-            TrayIconEvent::Enter {
-                id: _,
-                position: _,
-                rect: _,
-            } => {
-                tray.app_handle()
-                    .emit_to("notify", "notify_enter", &tray.rect().unwrap())
-                    .unwrap();
+        // 设置托盘图标菜单
+        .menu(&menu)
+        // 防止菜单在鼠标左键单击时弹出
+        .show_menu_on_left_click(false)
+        // 托盘菜单点击事件
+        .on_menu_event(|app, event| match event.id.as_ref() {
+            "quit" => {
+                app.exit(0);
             }
-            #[cfg(target_os = "windows")]
-            TrayIconEvent::Leave {
-                id: _,
-                position: _,
-                rect: _,
+            _ => {
+                println!("menu item {:?} not handled", event.id);
+            }
+        })
+        // 托盘图标响应鼠标事件
+        .on_tray_icon_event(|tray, event| match event {
+            // 鼠标左键双击事件
+            TrayIconEvent::DoubleClick {
+                button: MouseButton::Left,
+                ..
             } => {
-                tray.app_handle()
-                    .emit_to("notify", "notify_leave", ())
-                    .unwrap();
+                println!("left click pressed and released");
+                // in this example, let's show and focus the main window when the tray is clicked
+                let app = tray.app_handle();
+                if let Some(window) = app.get_webview_window("main") {
+                    let _ = window.show();
+                    let _ = window.set_focus();
+                }
             }
             _ => {}
         })
