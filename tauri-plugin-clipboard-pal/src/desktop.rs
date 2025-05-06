3e9c1ac2e8cda8f9f7cdec86df2c94144f165980
张晓 <1750777402@qq.com>
2025-05-06 09:57:19 +0800

    去除无用代码

--- tauri-plugin-clipboard-pal/src/desktop.rs
+++ tauri-plugin-clipboard-pal/src/desktop.rs
@@ -31,225 +31,7 @@ pub struct ClipboardPal {
 }
 
 impl ClipboardPal {
-    pub fn has(&self, format: ContentFormat) -> Result<bool, String> {
-        Ok(self
-            .clipboard
-            .lock()
-            .map_err(|err| err.to_string())?
-            .has(format))
-    }
-
-    pub fn available_types(&self) -> Result<AvailableTypes, String> {
-        Ok(AvailableTypes {
-            text: self.has(ContentFormat::Text)?,
-            html: self.has(ContentFormat::Html)?,
-            rtf: self.has(ContentFormat::Rtf)?,
-            image: self.has(ContentFormat::Image)?,
-            files: self.has(ContentFormat::Files)?,
-        })
-    }
-
-    pub fn has_text(&self) -> Result<bool, String> {
-        self.has(ContentFormat::Text)
-    }
-
-    pub fn has_rtf(&self) -> Result<bool, String> {
-        self.has(ContentFormat::Rtf)
-    }
-
-    pub fn has_image(&self) -> Result<bool, String> {
-        self.has(ContentFormat::Image)
-    }
-
-    pub fn has_html(&self) -> Result<bool, String> {
-        self.has(ContentFormat::Html)
-    }
-
-    pub fn has_files(&self) -> Result<bool, String> {
-        self.has(ContentFormat::Files)
-    }
-
-    // Read from Clipboard APIs
-
-    /// read text from clipboard
-    pub fn read_text(&self) -> Result<String, String> {
-        self.clipboard
-            .lock()
-            .map_err(|err| err.to_string())?
-            .get_text()
-            .map_err(|err| err.to_string())
-    }
-
-    pub fn read_html(&self) -> Result<String, String> {
-        self.clipboard
-            .lock()
-            .map_err(|err| err.to_string())?
-            .get_html()
-            .map_err(|err| err.to_string())
-    }
-
-    pub fn read_rtf(&self) -> Result<String, String> {
-        self.clipboard
-            .lock()
-            .map_err(|err| err.to_string())?
-            .get_rich_text()
-            .map_err(|err| err.to_string())
-    }
-
-    /// read files from clipboard and return a `Vec<String>`
-    /// Will return a vector of strings, in uri format: `file:///path/to/file`. File path is absolute path.
-    /// On Windows, the path will be in the format `C:\\path\\to\\file`. This method is the same as read_files on windows
-    pub fn read_files_uris(&self) -> Result<Vec<String>, String> {
-        let files = self
-            .clipboard
-            .lock()
-            .map_err(|err| err.to_string())?
-            .get_files()
-            .map_err(|err| err.to_string())?;
-        Ok(files)
-    }
-
-    /// read files from clipboard and return a `Vec<String>`
-    /// Will return a vector of strings, in absolute path format: `/path/to/file`.
-    /// On Windows, the path will be in the format `C:\\path\\to\\file`. This method is the same as read_files_uris on windows
-    pub fn read_files(&self) -> Result<Vec<String>, String> {
-        let files = self.read_files_uris()?;
-        // iterate through the files and remove the `file://` prefix if there is any. Only remove the prefix if it's in the beginning
-        let files_str = files
-            .iter()
-            .map(|file| {
-                if file.starts_with("file://") {
-                    file.strip_prefix("file://").unwrap().to_string()
-                } else {
-                    file.to_string()
-                }
-            })
-            .collect::<Vec<_>>();
-        Ok(files_str)
-    }
-
-    /// Write files uris to clipboard. The files should be in uri format: `file:///path/to/file` on Mac and Linux. File path is absolute path.
-    /// On Windows, the path should be in the format `C:\\path\\to\\file`.
-    pub fn write_files_uris(&self, files: Vec<String>) -> Result<(), String> {
-        // iterate through files, check if it starts with files://, if not throw error (only linux and mac)
-        #[cfg(any(target_os = "linux", target_os = "macos"))]
-        {
-            for file in &files {
-                if !file.starts_with("file://") {
-                    return Err(format!(
-                        "Invalid file uri: {}. File uri should start with file://",
-                        file
-                    ));
-                }
-            }
-        }
-        // On Windows, we don't need the file:// prefix, so we remove it if it's there
-        #[cfg(target_os = "windows")]
-        {
-            for file in &files {
-                if file.starts_with("file://") {
-                    return Err(format!(
-                        "Invalid file uri: {}. File uri on Windows should not start with file://",
-                        file
-                    ));
-                }
-            }
-        }
-
-        self.clipboard
-            .lock()
-            .map_err(|err| err.to_string())?
-            .set_files(files)
-            .map_err(|err| err.to_string())
-    }
-
-    /// read image from clipboard and return a base64 string
-    pub fn read_image_base64(&self) -> Result<String, String> {
-        let image_bytes = self.read_image_binary()?;
-        let base64_str = general_purpose::STANDARD.encode(image_bytes);
-        Ok(base64_str)
-    }
-
-    /// read image from clipboard and return a `Vec<u8>`
-    pub fn read_image_binary(&self) -> Result<Vec<u8>, String> {
-        let image = self
-            .clipboard
-            .lock()
-            .map_err(|err| err.to_string())?
-            .get_image()
-            .map_err(|err| err.to_string())?;
-        let bytes = image
-            .to_png()
-            .map_err(|err| err.to_string())?
-            .get_bytes()
-            .to_vec();
-        // let bytes = util::image_data_to_bytes(&image);
-        Ok(bytes)
-    }
-
-    // Write to Clipboard APIs
-    pub fn write_text(&self, text: String) -> Result<(), String> {
-        self.clipboard
-            .lock()
-            .map_err(|err| err.to_string())?
-            .set_text(text)
-            .map_err(|err| err.to_string())
-    }
-
-    pub fn write_html(&self, html: String) -> Result<(), String> {
-        self.clipboard
-            .lock()
-            .map_err(|err| err.to_string())?
-            .set_html(html)
-            .map_err(|err| err.to_string())
-    }
-
-    pub fn write_html_and_text(&self, html: String, text: String) -> Result<(), String> {
-        self.clipboard
-            .lock()
-            .map_err(|err| err.to_string())?
-            .set(vec![
-                ClipboardContent::Text(text),
-                ClipboardContent::Html(html),
-            ])
-            .map_err(|err| err.to_string())
-    }
-
-    pub fn write_rtf(&self, rtf: String) -> Result<(), String> {
-        self.clipboard
-            .lock()
-            .map_err(|err| err.to_string())?
-            .set_rich_text(rtf)
-            .map_err(|err| err.to_string())
-    }
-
-    /// write base64 png image to clipboard
-    pub fn write_image_base64(&self, base64_image: String) -> Result<(), String> {
-        let decoded = general_purpose::STANDARD
-            .decode(base64_image)
-            .map_err(|err| err.to_string())?;
-        self.write_image_binary(decoded)
-            .map_err(|err| err.to_string())?;
-        Ok(())
-    }
-
-    pub fn write_image_binary(&self, bytes: Vec<u8>) -> Result<(), String> {
-        let img = RustImageData::from_bytes(bytes.as_bytes()).map_err(|err| err.to_string())?;
-        self.clipboard
-            .lock()
-            .map_err(|err| err.to_string())?
-            .set_image(img)
-            .unwrap();
-        Ok(())
-    }
-
-    pub fn clear(&self) -> Result<(), String> {
-        self.clipboard.lock().unwrap().clear().unwrap();
-        Ok(())
-    }
-
     pub fn start_monitor<R: Runtime>(&self, app_handle: AppHandle<R>) -> Result<(), String> {
-        let _ = app_handle.emit("plugin:clipboard://clipboard-monitor/status", true);
         let clipboard = ClipboardMonitor::new(self.clipboard.clone());
         let mut watcher: ClipboardWatcherContext<ClipboardMonitor> =
             ClipboardWatcherContext::new().unwrap();
@@ -266,7 +48,6 @@ impl ClipboardPal {
     }
 
     pub fn stop_monitor<R: Runtime>(&self, app_handle: AppHandle<R>) -> Result<(), String> {
-        let _ = app_handle.emit("plugin:clipboard://clipboard-monitor/status", false);
         let mut watcher_shutdown_state = self.watcher_shutdown.lock().unwrap();
         if let Some(watcher_shutdown) = (*watcher_shutdown_state).take() {
             watcher_shutdown.stop();
@@ -292,13 +73,44 @@ impl ClipboardMonitor {
 
 impl ClipboardHandler for ClipboardMonitor {
     fn on_clipboard_change(&mut self) {
-        let text = self
+        println!("");
+        let clipboard_context = self
             .clipboard
             .lock()
             .map_err(|err| err.to_string())
-            .unwrap()
-            .get_text()
-            .map_err(|err| err.to_string());
-        println!("复制了{:?}", text);
+            .unwrap();
+
+        if clipboard_context.has(ContentFormat::Text) {
+            let text_context = clipboard_context.get_text().map_err(|err| err.to_string());
+            if let Ok(text) = text_context {
+                println!("复制了文本{:?}", text);
+            }
+        }
+        if clipboard_context.has(ContentFormat::Image) {
+            let text_context = clipboard_context.get_image().map_err(|err| err.to_string());
+            if let Ok(image) = text_context {
+                println!("复制了图片");
+            }
+        }
+        if clipboard_context.has(ContentFormat::Rtf) {
+            let text_context = clipboard_context
+                .get_rich_text()
+                .map_err(|err| err.to_string());
+            if let Ok(content) = text_context {
+                println!("复制了富文本内容:{}", content);
+            }
+        }
+        if clipboard_context.has(ContentFormat::Html) {
+            let text_context = clipboard_context.get_html().map_err(|err| err.to_string());
+            if let Ok(content) = text_context {
+                println!("复制了html内容:{}", content);
+            }
+        }
+        if clipboard_context.has(ContentFormat::Files) {
+            let text_context = clipboard_context.get_files().map_err(|err| err.to_string());
+            if let Ok(content) = text_context {
+                println!("复制了文件:{:?}", content);
+            }
+        }
     }
 }
